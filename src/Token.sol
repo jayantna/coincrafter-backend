6853ee89eeb461c3672ada4b5132c2d38b105b88
jayantna <jonty.nagle@gmail.com>
2025-02-17 19:21:33 +0530

    Make dynamic tokens

--- src/Token.sol
+++ src/Token.sol
@@ -1,10 +1,11 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Token", "TKN") {
+    constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol)
+    ERC20(tokenName, tokenSymbol) {
         _mint(msg.sender, initialSupply);
     }
 }
