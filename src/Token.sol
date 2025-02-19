0203d62296a4e2e37dadc40d0ae4c7e1e2bebafe
jayantna <jonty.nagle@gmail.com>
2025-02-19 18:47:40 +0530

    Making msg.sender of Craft contract as owner of token

--- src/Token.sol
+++ src/Token.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.19;
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor(uint256 initialSupply, string memory tokenName, string memory tokenSymbol)
+    constructor(address owner, uint256 initialSupply, string memory tokenName, string memory tokenSymbol)
         ERC20(tokenName, tokenSymbol)
     {
-        _mint(msg.sender, initialSupply);
+        _mint(owner, initialSupply);
     }
 }
